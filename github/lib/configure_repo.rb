6a35eedab6ccb8957dfc48eb6faf921ff6e9d2dd
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2020-09-21 15:55:48 +0100

    push

--- github/lib/configure_repo.rb
+++ github/lib/configure_repo.rb
@@ -94,7 +94,7 @@ private
 
     if jenkinsfile_exists?
       if existing_webhooks.map(&:config).map(&:url).include?("https://ci.blue.integration.govuk.digital/github-webhook/")
-        puts "config: #{existing_webhooks.map(&:config)}"
+        puts "config: #{existing_webhooks}"
         client.remove_hook(repo[:full_name], existing_webhooks.map(&:config).map(&:id))
       end
     end
